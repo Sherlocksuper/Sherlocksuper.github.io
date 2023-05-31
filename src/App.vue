14f4191491b117bb138277cf853fb504902df036
Sherlock <1075773551@qq.com>
2023-05-31 17:13:01 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script >
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello World</h1>
 </template>
 
-<style scoped>
+<style>
 header {
   line-height: 1.5;
 }
